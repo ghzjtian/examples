f6d1d97c7b9eb85238c301e3ad630a0827801213
TianZeng <jingtian.zeng@globetools.com>
2021-09-07 14:36:50 +0800

    Learn bodyParser

--- bodyParser/config/config.default.js
+++ bodyParser/config/config.default.js
@@ -1,11 +1,28 @@
 'use strict';
 
-exports.keys = '123456';
+// exports.keys = '123456';
 
-exports.bodyParser = {
-  enableTypes: [ 'json', 'form', 'text' ],
-  extendTypes: {
-    json: 'application/custom-json',
-    text: [ 'application/xml' ],
+// exports.bodyParser = {
+//   enableTypes: ['json', 'form', 'text'],
+//   extendTypes: {
+//     json: 'application/custom-json',
+//     text: ['application/xml'],
+//   },
+// };
+
+module.exports = {
+  keys: '123456',
+  bodyParser: {
+    enableTypes: ['json', 'form', 'text'],
+    extendTypes: {
+      json: 'application/custom-json',
+      text: ['application/xml'],
+    },
+  },
+  // 关闭 csrf 校验.
+  security: {
+    csrf: {
+      enable: false,
+    },
   },
 };
